adda7edab3d5572598a065ab546cd5eeced1954e
Luiz Eduardo Santafosta <dusantafosta@hotmail.com>
2024-09-04 16:36:08 -0300

    Fundamentos do ASP.NET MVC
    Projetos do segundo curso "Fundamentos do ASP.NET MVC"

---
+++ FundamentosAspNetMvc/MinhaAppFuncional/src/AppMvcFuncional/Program.cs
@@ -0,0 +1,50 @@
+using AppMvcFuncional.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+// Entity Framework
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Filtro de erros de BD (Somente desenvolvimento)
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Identity
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// MVC
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
---
+++ FundamentosAspNetMvc/PrimeiraApp/ViewComponents/SaudacaoAlunoViewComponent.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.ViewComponents;
+
+public class SaudacaoAlunoViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        // Pegar o aluno na base de dados
+        // Pegar o dado (nome) do aluno que está logado
+        var aluno = new Aluno() { Nome = "Eduardo" };
+
+        return View(aluno);
+    }
+}
