73fa9585d95931066bfc2ff363f20d9903e66059
Luiz Eduardo Santafosta <dusantafosta@hotmail.com>
2025-02-03 17:36:54 -0300

    Components, Modules e Decorators

---
+++ DesenvolvimentoSPAComAngular/MinhaAppAngular/src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    // Componentes
+  ],
+  imports: [
+    CommonModule
+    // Módulos
+  ],
+  providers: [
+    //Serviços
+  ]
+})
+export class FuncionalidadeModule { }
---
+++ DesenvolvimentoSPAComAngular/MinhaAppAngular/src/app/pasta/nome-componente/nome-componente.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nome-componente',
+  imports: [],
+  templateUrl: './nome-componente.component.html'
+})
+export class NomeComponenteComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    
+  }
+}
