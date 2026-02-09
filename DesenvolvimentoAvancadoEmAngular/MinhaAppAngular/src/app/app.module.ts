ed7463505b151cebaf11501372b0a48f90ca03ae
Luiz Eduardo Santafosta <dusantafosta@hotmail.com>
2026-02-09 10:07:34 -0300

    09/02/2026

--- DesenvolvimentoAvancadoEmAngular/MinhaAppAngular/src/app/app.module.ts
+++ DesenvolvimentoAvancadoEmAngular/MinhaAppAngular/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Provider } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { NgBrazil } from 'ng-brazil'
 import { TextMask } from 'ng-brazil';
 import { CustomFormsModule } from 'ng2-validation'
+
+import { registerLocaleData } from "@angular/common";
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 
 import { AppComponent } from './app.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
@@ -17,13 +22,23 @@ import { AppRoutingModule } from './app.routes';
 import { AuthGuard } from './services/app.guard';
 import { CadastroGuard } from './services/cadastro.guard';
 import { FilmesComponent } from './demos/pipes/filmes/filmes.component';
+import { FileSizePipe } from './demos/pipes/filmes/filesize.pipe';
+import { ImageFormaterPipe } from './demos/pipes/filmes/image.pipe';
+import { BarModule } from './demos/bar-di-zones/bar.module';
+import { BarServices } from './demos/bar-di-zones/bar.service';
+
+export const BAR_PROVIDERS: Provider[] = [
+  BarServices
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SobreComponent,
     CadastroComponent,
-    FilmesComponent
+    FilmesComponent,
+    FileSizePipe,
+    ImageFormaterPipe
   ],
   imports: [
     BrowserModule,
@@ -33,11 +48,17 @@ import { FilmesComponent } from './demos/pipes/filmes/filmes.component';
     TextMask.TextMaskModule,
     NgBrazil,
     CustomFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BarModule.forRoot({
+      unidadeId: 1000,
+      unidadeToken: 'eca938c99a8e9ff91029dc'
+    })
   ],
   providers: [
     AuthGuard,
-    CadastroGuard
+    CadastroGuard,
+    //BAR_PROVIDERS
   ],
   bootstrap: [AppComponent]
 })
--- DesenvolvimentoAvancadoEmAngular/MinhaAppAngular/src/app/demos/pipes/filmes/filmes.component.ts
+++ DesenvolvimentoAvancadoEmAngular/MinhaAppAngular/src/app/demos/pipes/filmes/filmes.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Filme } from './filme';
+import { ImageFormaterPipe } from './image.pipe';
 
 @Component({
   selector: 'app-filmes',
   templateUrl: './filmes.component.html',
+  providers: [
+    ImageFormaterPipe
+  ]
 })
 export class FilmesComponent implements OnInit {
 
-  filmes: Filme[]
+  filmes: Filme[];
+  mapped: Filme[];
+
+  constructor(private imagemFormat: ImageFormaterPipe){}
 
   ngOnInit() {
     
@@ -48,6 +55,16 @@ export class FilmesComponent implements OnInit {
         tamanho: '773039680'
       }
     ]
+
+    this.mapped = this.filmes.map(filme => {
+      return {
+        nome: filme.nome,
+        dataLancamento: filme.dataLancamento,
+        valor: filme.valor,
+        tamanho: filme.tamanho,
+        imagem: this.imagemFormat.transform(filme.imagem, 'default', true)
+      }
+    });
   }
 
 }
